42a64a60a5bcfe3246d5b05d5fd772c782967fb2
Joao Lucas <joao.domingos@sou.unifeob.edu.br>
2024-09-24 23:40:37 -0300

    tela-login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirecionar para login na raiz
+  { path: 'login', component: LoginComponent }, // Definir a rota de login
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
