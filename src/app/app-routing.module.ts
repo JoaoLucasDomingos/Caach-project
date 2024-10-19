fbc0d14c7e51af3f13bf331994b3b59ea9342a44
Joao Lucas <joao.domingos@sou.unifeob.edu.br>
2024-10-18 21:26:19 -0300

    corregindoe_erros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,16 @@ import { DoadorComponent } from './components/doador/doador.component';
 import { DoadorListComponent } from './components/doador-list/doador-list.component';
 import { DoadorEditComponent } from './components/doador-edit/doador-edit.component';
 
-
-
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, 
-  { path: 'login', component: LoginComponent }, 
-  { path: 'home', component:HomeComponent},
-  { path: 'doador', component:DoadorComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redireciona de '' para '/login'
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'doador', component: DoadorComponent },
   { path: 'doador-list', component: DoadorListComponent },
   { path: 'doador/edit/:cpf', component: DoadorEditComponent }, // Rota para edição
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'doador/edit/:cpf', component: DoadorEditComponent },
+  // Remova a linha duplicada abaixo
+  // { path: 'doador/edit/:cpf', component: DoadorEditComponent }, 
+  { path: '**', redirectTo: '/login' } // Redireciona qualquer rota desconhecida para '/login'
 ];
 
 @NgModule({
